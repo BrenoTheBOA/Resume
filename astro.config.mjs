a70ae9b329633f8845d1867f5318582540f5df2c
Breno de Oliveira Almeida <brenobelga@live.com>
2026-09-21 18:59:56 -0300

    Make Astro base path environment-aware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from "astro/config";
-export default defineConfig({ site: "https://brenotheboa.github.io", base: "/Resume" });
+
+export default defineConfig({
+  site: "https://brenotheboa.github.io",
+  base: process.env.BASE_PATH || "/"
+});
